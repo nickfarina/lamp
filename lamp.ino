454cec9b2e13096e848eff79c5706048f3d055ad
Nick Farina <farina.nick@gmail.com>
2019-08-02 11:33:19 -0400

    set up basic capacitor sensor

--- lamp.ino
+++ lamp.ino
@@ -1,12 +1,23 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor sensorA = CapacitiveSensor(4,2); // 1 megohm resistor between pins 4 & 2,
+                                                 // pin 2 is sensor pin, add wire, foil
+long baseline = 30;
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
+
+  sensorA.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibrate on channel 1 - just as an example
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(10);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(10);                       // wait for a second
+  long sensorAReading = sensorA.capacitiveSensor(30);
+
+  Serial.println(sensorAReading);
+
+  delay(10);
 }
