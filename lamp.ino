a81c64e378e217525b97dc350983428792f61dd9
Nick Farina <farina.nick@gmail.com>
2019-08-02 11:50:23 -0400

    roughly sense hovering and touching

--- lamp.ino
+++ lamp.ino
@@ -2,7 +2,11 @@
 
 CapacitiveSensor sensorA = CapacitiveSensor(4,2); // 1 megohm resistor between pins 4 & 2,
                                                  // pin 2 is sensor pin, add wire, foil
-long baseline = 30;
+long baseline = 0;
+int calibrationCycles = 500;
+
+int HOVER_THRESHOLD = 20;
+int TOUCH_THRESHOLD = 100;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -10,14 +14,38 @@ void setup() {
 
   Serial.begin(9600);
 
-  sensorA.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibrate on channel 1 - just as an example
+  sensorA.set_CS_AutocaL_Millis(0xFFFFFFFF);
+
+  calibrate();
 }
 
 // the loop function runs over and over again forever
 void loop() {
   long sensorAReading = sensorA.capacitiveSensor(30);
+  long change = (sensorAReading - baseline);
 
-  Serial.println(sensorAReading);
+//  Serial.println(change);
 
-  delay(10);
+  
+  if (change > TOUCH_THRESHOLD) {
+    Serial.println("Touching!!");
+  } else if (change > HOVER_THRESHOLD) {
+    Serial.println("Hovering!");
+  }
+}
+
+void calibrate() {
+  int sum= 0;
+  
+  for (int i = 0; i < calibrationCycles ; i++) {
+    long sensorAReading = sensorA.capacitiveSensor(30);
+    sum += sensorAReading;
+    Serial.println(sensorAReading);
+  }
+
+  baseline = sum / calibrationCycles;
+  
+  for (int i = 0; i < 30; i++) {
+    Serial.println(baseline);
+  } 
 }
