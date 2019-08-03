da1f7417f32dc05158a7451ebfbe19beb592d089
Nick Farina <farina.nick@gmail.com>
2019-08-03 14:23:29 -0400

    find sensor min and max on startup

--- lamp.ino
+++ lamp.ino
@@ -5,11 +5,14 @@ CapacitiveSensor sensorA = CapacitiveSensor(4,2); // 1 megohm resistor between p
 
 int LED_PIN = 9;
 
+// Calibration
 long baseline = 0;
 int calibrationCycles = 500;
+int sensorMin = 1000;
+int sensorMax = 0;
 
-int HOVER_THRESHOLD = 20;
-int TOUCH_THRESHOLD = 30;
+int HOVER_THRESHOLD = 80;
+int TOUCH_THRESHOLD = 19;
 
 // State Variables
 boolean HOVERING = false;
@@ -29,12 +32,15 @@ void setup() {
   sensorA.set_CS_AutocaL_Millis(0xFFFFFFFF);
 
   calibrate();
+  Serial.println("calibrated");
 }
 
 void loop() {
   long sensorAReading = sensorA.capacitiveSensor(30);
-  long change = (sensorAReading - baseline);
+  long change = (sensorAReading - sensorMax);
+  
   Serial.println(change);
+  
   detectInteraction(change);
   toggleLightState();
 
@@ -47,18 +53,18 @@ void adjustLights() {
   // adjust lights
   if (lightState == HIGH && TOUCHING) {
     if (lightValue < 255) {
-      if (lightValue == 0) {
-        lightValue = 5;
-      } else {
+//      if (lightValue == 0) {
+//        lightValue = 5;
+//      } else {
         lightValue++;
-      }
+//      }
     }
   } else if (lightState == LOW) {
-    if (lightValue <= 10 && lightValue > 0) {
-      lightValue--;
-    } else {
+//    if (lightValue <= 10 && lightValue > 0) {
+//      lightValue--;
+//    } else {
       lightValue = 0;
-    }
+//    }
   }
 
   analogWrite(LED_PIN, lightValue);
@@ -96,10 +102,22 @@ void calibrate() {
   
   for (int i = 0; i < calibrationCycles ; i++) {
     long sensorAReading = sensorA.capacitiveSensor(30);
-    sum += sensorAReading;
-  }
+    
+    if (sensorAReading > sensorMax) {
+      sensorMax = sensorAReading;
+    }
 
-  baseline = sum / calibrationCycles;
+    if (sensorAReading < sensorMin) {
+      sensorMin = sensorAReading;
+    }
+  }
   
-  Serial.println("baseline: " + baseline);
+  pp("min: ", sensorMin);
+  pp("max: ", sensorMax);
+}
+
+// pretty print utility
+void pp(String str, int num) {
+  Serial.print(str);
+  Serial.println(num);
 }
